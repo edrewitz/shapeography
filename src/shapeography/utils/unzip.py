fcd29e51ba142ae502c425a975e47c4ef5cc8a15
edrewitz <drewitzwx@gmail.com>
2026-02-16 18:02:03 -0800

    Added .tar.gz zip

--- src/shapeography/utils/unzip.py
+++ src/shapeography/utils/unzip.py
@@ -6,77 +6,12 @@ This file hosts the functions responsible for unzipping the shapefiles
 
 import os as _os
 import gzip as _gzip
+import tarfile as _tarfile
 
 from zipfile import ZipFile as _ZipFile
     
-def extract_gzipped_files(file_directory,
-                          file_extention='.shp'):
-    
-    """
-    This function extracts shapefiles that are zipped in a .gz file
-    
-    Required Arguments:
-    
-    1) file_directory (String) - The path of the directory to the initial .gz file. 
-    
-    Optional Arguments: 
-    
-    1) file_extention (String) - Default='.shp'. .shp is the standard file extention for a shapefile. 
-
-        Other Common Cartographic File Extentions
-        -----------------------------------------
-        
-        .geojson - GEOJSON
-    
-    Returns
-    -------
-    
-    A directory of unzipped shapefiles. 
-    """
-
-    try:
-        for f in _os.listdir(f"{file_directory}"):
-            extraction_folder = f.split('.', 1)[0]
-            with _gzip.open(f"{file_directory}/{f}", 'rb') as f_in:
-                with open(f"{file_directory}/{extraction_folder}", 'wb') as f_out:
-                    f_out.write(f_in.read())
-    except Exception as e:
-        pass
-    
-    if file_extention is not None:
-        try:
-            for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
-                ex_folder = f.split('.', 1)[0]
-                with _gzip.open(f"{file_directory}/{extraction_folder}/{f}", 'rb') as f_in:
-                    with open(f"{file_directory}/{extraction_folder}/{ex_folder}{file_extention}", 'wb') as f_out:
-                        f_out.write(f_in.read()) 
-        except Exception as e:
-            pass     
-    else:
-        try:
-            for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
-                ex_folder = f.split('.', 1)[0]
-                with _gzip.open(f"{file_directory}/{extraction_folder}/{f}", 'rb') as f_in:
-                    with open(f"{file_directory}/{extraction_folder}/{ex_folder}", 'wb') as f_out:
-                        f_out.write(f_in.read()) 
-        except Exception as e:
-            pass            
-    
-    try:    
-        for f in _os.listdir(f"{file_directory}"):
-            if f.endswith(".gz"):
-                _os.remove(f"{file_directory}/{f}")
-    except Exception as e:
-        pass
-    
-    try:    
-        for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
-            if f.endswith(".gz"):
-                _os.remove(f"{file_directory}/{extraction_folder}/{f}")
-    except Exception as e:
-        pass     
-    
-def extract_zipped_files(file_directory):
+def extract_zipped_files(file_directory,
+                         file_type='.zip'):
 
     """
     This function extracts shapefiles that are zipped in a .zip file
@@ -93,33 +28,54 @@ def extract_zipped_files(file_directory):
     A directory of unzipped shapefiles. 
     """
     
-    try:
-        for f in _os.listdir(f"{file_directory}"):
-            extraction_folder = f.split('.', 1)[0]
-            with _ZipFile(f"{file_directory}/{f}", 'r') as zObject:
-                zObject.extractall(f"{file_directory}/{extraction_folder}")
-            zObject.close()
-    except Exception as e:
-        pass
+    file_type = file_type.lower()
     
-    try:    
-        for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
-            ex_folder = f.split('.', 1)[0]
-            with _ZipFile(f"{file_directory}/{extraction_folder}/{f}", 'r') as zObject:
-                zObject.extractall(f"{file_directory}/{extraction_folder}/{ex_folder}")
-            zObject.close()   
-    except Exception as e:
-        pass     
+    if file_type == '.zip':
+        try:
+            for f in _os.listdir(f"{file_directory}"):
+                extraction_folder = f.split('.', 1)[0]
+                with _ZipFile(f"{file_directory}/{f}", 'r') as zObject:
+                    zObject.extractall(f"{file_directory}/{extraction_folder}")
+                zObject.close()
+        except Exception as e:
+            pass
+        
+        try:    
+            for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
+                ex_folder = f.split('.', 1)[0]
+                with _ZipFile(f"{file_directory}/{extraction_folder}/{f}", 'r') as zObject:
+                    zObject.extractall(f"{file_directory}/{extraction_folder}/{ex_folder}")
+                zObject.close()   
+        except Exception as e:
+            pass 
+    
+    elif file_type == '.tar.gz':
+        try:
+            for f in _os.listdir(f"{file_directory}"):
+                extraction_folder = f.split('.', 1)[0]
+                with _tarfile.open(f"{file_directory}/{f}", "r:gz") as tar:
+                    tar.extractall(path=f"{file_directory}/{extraction_folder}")
+        except Exception as e:
+            pass
+        
+        try:
+            for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
+                ex_folder = f.split('.', 1)[0]
+                with _tarfile.open(f"{file_directory}/{extraction_folder}/{f}", "r:gz") as tar:
+                    tar.extractall(path=f"{file_directory}/{extraction_folder}/{ex_folder}")
+        except Exception as e:
+            pass
+        
     try:    
         for f in _os.listdir(f"{file_directory}"):
-            if f.endswith(".zip"):
+            if f.endswith(file_type):
                 _os.remove(f"{file_directory}/{f}")
     except Exception as e:
         pass
     
     try:    
         for f in _os.listdir(f"{file_directory}/{extraction_folder}"):
-            if f.endswith(".zip"):
+            if f.endswith(file_type):
                 _os.remove(f"{file_directory}/{extraction_folder}/{f}")
     except Exception as e:
         pass
