c8c631bd4184368734dd6e3c8efcaa85e8e23b2e
edrewitz <drewitzwx@gmail.com>
2026-02-17 15:06:14 -0800

    Added docstring to __init__.py

--- src/shapeography/__init__.py
+++ src/shapeography/__init__.py
@@ -1,3 +1,11 @@
+"""
+This file hosts all of the functions in the shapeography package that directly interact with the user. 
+
+(C) Eric J. Drewitz 2026
+"""
+
+
+
 import shapeography.utils.geometry as geometry
 import shapeography.utils.unzip as unzip
 import shapeography.client.client as client
